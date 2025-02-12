0607ca01e1897750778816a208a7e41e75ebbd1a
SA05 <sa05@SAs-MacBook-Air.local>
2025-02-12 18:13:26 -0500

    renovationweb

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,12 +1,16 @@
 alert("Hiiiiii")
 
-
 let age = 16;
 if (age >= 18){
-    console.log('You are a adult');
+    console.log('You are an adult');
 }
 else{
     console.log('Stay in school!')
 }
 const birthYear = 2008;
 console.log("My age is ", age)
+
+let myheading = document.getElementById("my heading");
+
+myheading.innerHTML = "Welcome to Mathematics";
+myheading.style.fontSize = "3rem";  
