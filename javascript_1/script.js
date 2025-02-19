7f1b80ce2ea1f5183c5453814d8ce74632a28a35
SA05 <sa05@SAs-MacBook-Air.local>
2025-02-19 18:03:09 -0500

    new

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,16 +1,17 @@
-alert("Hiiiiii")
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2; 
+console.log("The sum of", num1, "and", num2, "is", sum); 
+
+let resultElement = document.getElementById('result'); 
+resultElement.innerHTML = "The sum of " + num1 + " and " + num2 + " is: " + sum;
 
 let age = 16;
-if (age >= 18){
-    console.log('You are an adult');
-}
-else{
-    console.log('Stay in school!')
-}
 const birthYear = 2008;
-console.log("My age is ", age)
 
-let myheading = document.getElementById("my heading");
+if (age >= 18 && birthYear <= 2007) {
+    console.log('You are an adult and born before 2007');
+} else {
+    console.log('Either you are not an adult or you were born after 2007');
+}
 
-myheading.innerHTML = "Welcome to Mathematics";
-myheading.style.fontSize = "3rem";  
